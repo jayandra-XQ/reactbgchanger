2afa60336b2eb1cd3abd3a453f24c822c373ef2a
Jayandra Hamal <salonhamal278@gmail.com>
2023-11-04 21:21:37 +0530

    bgchange complete

--- src/App.jsx
+++ src/App.jsx
@@ -14,37 +14,43 @@ function App() {
         <div className="flex flex-wrap  justify-center
         gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-          onClick={() => setColor('red')}
+            onClick={() => setColor('red')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "red" }}
           >Red</button>
 
           <button
+            onClick={() => setColor('green')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "green" }}
           >Green</button>
 
           <button
+            onClick={() => setColor('purple')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "purple" }}
           >Purple</button>
 
           <button
+            onClick={() => setColor('black')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "black" }}
           >Black</button>
 
           <button
+            onClick={() => setColor('blue')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "blue" }}
           >Blue</button>
 
           <button
+            onClick={() => setColor('violet')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "violet" }}
           >Violet</button>
 
           <button
+            onClick={() => setColor('yellow')}
             className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
             style={{ backgroundColor: "yellow" }}
           >Yellow</button>
